02664e7e9f33f4313a2020608cdfad65d7a4c9de
GVignesh-SD <gvignesh380980@gmail.com>
2023-06-15 22:43:30 +0530

    car showcase project final

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,11 +25,7 @@ const Hero = () => {
           process.
         </p>
 
-        <CustomButton
-          title="Explore Cars"
-          containerStyles="bg-primary-blue text-white rounded-full mt-10"
-          handleClick={handleScroll}
-        />
+        
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
