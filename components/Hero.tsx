354cebd855e6b5e29e37bdc10dc127118f7a06f0
GVignesh-SD <gvignesh380980@gmail.com>
2023-06-16 23:25:03 +0530

    car showcase project final

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,14 @@ import Image from "next/image";
 
 import { CustomButton } from "@components";
 
+const Hero = () => {
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
 
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
     <div className="hero">
